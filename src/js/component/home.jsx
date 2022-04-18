0cc80131b30b45c7faaaaefae94afc61182a7f1a
jsoares07 <jfernandosoares7@gmail.com>
2022-04-18 14:58:45 +0000

    Added the main code

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -3,14 +3,16 @@ import reactDom from "react-dom";
 
 //create your first component
 const Home = () => {
-	const [newTask, setNewTask] = useState("");
-	const [taskList, setTaskList] = useState([]);
+	const [newTask, setNewTask] = useState(""); // ("") becuase we are expecting strings
+	const [taskList, setTaskList] = useState([]); // ([]) because we are expecting arrays when we add the tasks
 
 	function add(e) {
 		if (e.key === "Enter") {
 			if (newTask === "") {
+				//This will show an alert if the input is empty  and we press the Enter key
 				alert("You have to add a new task");
 			} else {
+				//This will link the task list with the use state with the list.
 				setTaskList((arr) => [...arr, newTask]);
 				setNewTask("");
 				return;
@@ -20,10 +22,11 @@ const Home = () => {
 	}
 
 	function erase(i) {
+		// This function will erase the tasks added to the list
 		if (i > -1) {
 			//validate that the array is not empty
-			let aux = taskList.filter((value, index) => index !== i);
-			setTaskList(aux);
+			let deleteTask = taskList.filter((value, item) => item !== i);
+			setTaskList(deleteTask);
 		}
 	}
 
@@ -33,38 +36,43 @@ const Home = () => {
 			<ul className="list-group shadow">
 				<input
 					className="list-group-item full-box"
-					id="inputTarea"
+					id="input-task"
 					placeholder="Type a new task"
 					onChange={(e) => setNewTask(e.target.value)}
 					value={newTask}
+					//Calling the add function in order the add whatever we typy in the input
 					onKeyPress={(e) => add(e)}></input>
+
 				{taskList.length === 0 ? (
+					//We create a conditional in case there is nothing (0) in the list(array)
 					<li className="list-group-item full-box">
 						<p className="my-2">No tasks, add a task</p>
 					</li>
 				) : (
-					taskList.map(function (name, index) {
+					//else we map the list/array to get whattever we type and keep adding items
+					taskList.map(function (task, item) {
 						return (
 							<li
-								key={index}
+								key={item}
 								className="list-group-item full-box d-flex justify-content-between align-items-center selected">
-								<p className="my-2">{name}</p>
+								<p className="my-2">{task}</p>
+								{/* We add the button to delete */}
 								<button
 									type="button"
-									className="btn btn-link hide"
-									onClick={() => {
-										erase(index);
-									}}>
+									className="btn btn-link hide">
 									<i className="fas fa-times"></i>
 								</button>
 							</li>
 						);
 					})
 				)}
+
+				{/*We create another conditional in wich we take the previous taskList.length === 0 to show the same string of "No tasks, add a task" */}
 				{taskList.length === 0 ? (
 					""
 				) : (
-					<li className="list-group-item full-box numero-items py-0">
+					//else,will create a <li> showing 2 different strings depending in how many items the list has.
+					<li className="list-group-item full-box number-items py-0">
 						<p className="my-2">
 							{taskList.length === 1
 								? taskList.length + " task to do"
